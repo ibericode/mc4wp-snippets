bd18cff7b66ca6d90d83cbe203ebb6c3f6e9855d
Danny van Kooten <dannyvankooten@gmail.com>
2016-02-16 16:48:46 +0100

    Add example that grabs values from submitted form

--- forms/email-notification-from-email.php
+++ forms/email-notification-from-email.php
@@ -2,8 +2,24 @@
 
 /**
  * Sets the email address & name to send the form notification email from.
+ *
+ * This example just sets a static name & email address
  */
 add_filter( 'mc4wp_form_email_notification_headers', function( $headers ) {
 	$headers['From'] = 'John Doe <johndoe@outlook.com>';
 	return $headers;
 });
+
+
+// -- //
+
+
+/**
+ * Sets the email address & name to send the form notification email from.
+ *
+ * This example uses the values from the "FNAME" and "EMAIL" field
+ */
+add_filter( 'mc4wp_form_email_notification_headers', function( $headers, $form ) {
+	$headers['From'] = sprintf( '%s <%s>', $form->data['FNAME'], $form->data['EMAIL'] );
+	return $headers;
+}, 10, 2 );
