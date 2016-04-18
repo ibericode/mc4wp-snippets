54ad3b63e507bd9f7f401d8c59a8f240b98909a7
Danny van Kooten <dannyvankooten@gmail.com>
2016-04-18 11:46:21 +0200

    Add WooCommerce Subscriptions example for MailChimp User Sync

---
+++ add-ons/user-sync/woocommerce-subscriptions.php
@@ -0,0 +1,7 @@
+<?php
+
+// only sync users with an active WooSubscription
+add_filter( 'mailchimp_sync_should_sync_user', function( $should, $user ) {
+    $active = WC_Subscriptions_Manager::user_has_subscription( $user->ID );
+    return $should && $active;
+});
