056e5b06f9ea84b3985382a32761a197df0b05a7
Danny van Kooten <dannyvankooten@gmail.com>
2016-05-02 11:08:40 +0200

    Add plugin headers to snippet for disabling log functionality.

--- premium/disable-functionality.php
+++ premium/disable-functionality.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Plugin Name: MailChimp for WordPress - Disable Reports Log
+Plugin URI: https://mc4wp.com/
+Description: Disabled the Reports > Log section in MailChimp for WordPress Premium.
+Author: ibericode
+Version: 1.0
+Author URI: https://ibericode.com/
+*/
+
 
 /**
  * This disables the "logging" functionality in the Premium add-on.
