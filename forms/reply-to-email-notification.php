d19f96064b052f12b6d9b3c30f0e71525bdb728e
Danny van Kooten <dannyvankooten@gmail.com>
2016-05-12 11:58:40 +0200

    Add snippet for setting `Reply-To` header for email notification.

---
+++ forms/reply-to-email-notification.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * This will set the header of the email notification to the person filling in the form.
+ */
+add_filter( 'mc4wp_form_email_notification_headers', function( $headers, $form ) {
+    $headers['Reply-To'] = $form->data['EMAIL'];
+    return $headers;
+}, 10, 2 );
