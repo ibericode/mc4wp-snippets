8ddec3be1e46616d65b8d5d2ca123065a6f377d9
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-28 13:54:02 +0200

    Update MailChimp Sync data example.

--- add-ons/user-sync/groupings.php
+++ add-ons/user-sync/groupings.php
@@ -1,21 +1,15 @@
 <?php
 
 /**
- * Adds the user to "Group 1" and "Group 2" of the Interest Grouping with ID 5405
+ * Adds the user to 2 interests
  *
- * @param array $data The data that is sent to MailChimp
+ * @param MC4WP_MailChimp_Subscriber $subscriber The data that is sent to MailChimp
  * @param WP_User $user The user that is being synchronized
  *
  * @return array Our modified data array
  */
-function myprefix_add_grouping_data( array $data, WP_User $user ) {
-	$grouping_1 = array(
-		'id' => 5405,
-		'groups' => array( 'Group 1', 'Group 2' )
-	);
-
-	$data['GROUPINGS'] = array( $grouping_1 );
-	return $data;
-}
-
-add_filter( 'mailchimp_sync_user_data', 'myprefix_add_grouping_data', 10, 2 );
+add_filter( 'mailchimp_sync_subscriber_data', function( $subscriber, $user ) {
+    $subscriber->interests[ "interest-id-1" ] = true;
+    $subscriber->interests[ "interest-id-2" ] = true;
+    return $subscriber;
+}, 10, 2 );
