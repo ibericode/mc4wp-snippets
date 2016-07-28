367d76015221757da2ed171be98dd1e3e31c3b27
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-28 13:58:14 +0200

    Update Restrict Content Pro example.

--- integrations/restrict-content-pro.php
+++ integrations/restrict-content-pro.php
@@ -13,7 +13,7 @@ add_action( 'rcp_form_processing', 'rcp_mailchimp_save_profile_fields', 10, 2 );
 add_action( 'rcp_user_profile_updated', 'rcp_mailchimp_save_profile_fields', 10 );
 
 add_filter( 'mailchimp_sync_should_sync_user', 'rcp_mailchimp_should_subscribe', 10, 2 );
-add_filter( 'mailchimp_sync_user_data', 'rcp_mailchimp_user_data', 10, 2 );
+add_filter( 'mailchimp_sync_subscriber_data', 'rcp_mailchimp_user_data', 10, 2 );
 
 function rcp_mailchimp_add_profile_fields( $user_id = 0 ) {
     $selected = $user_id && get_user_meta( $user_id, 'rcp_mailchimp', true );
@@ -51,29 +51,28 @@ function rcp_mailchimp_should_subscribe( $should, $user ) {
     return $should;
 }
 
-function rcp_mailchimp_user_data( $data, $user ) {
+function rcp_mailchimp_subscriber_data( MC4WP_MailChimp_Subscriber $subscriber, WP_User $user ) {
 
-    $grouping = array(
-        'id'        => 2671,    // the interest grouping id
-        'groups'    => array()  // the groups to add this user to
-    );
-
+    // map of RCP subscription ID => MailChimp interest ID
     $map = array(
-        '1' => 'Trade Membership',
-        '2' => 'Artist Membership',
-        '3' => 'Associate Membership',
-        '4' => 'Corporate Membership',
-        '5' => 'Supporter Membership',
+        '1' => "interest-id-1",     // Trade Membership
+        '2' => "interest-id-2",     // Artist Membership
+        '3' => "interest-id-3",     // Associate Membership
+        '4' => "d8v1k2ilx",         // Corporate Membership
+        '5' => "a0192m3az",         // Supporter Membership
     );
 
+    // get RCP subscription ID
     $subscription_id = rcp_get_subscription_id( $user->ID );
+
+    // see if this maps to a known interest
     if( isset( $map[ $subscription_id ] ) ) {
-        $grouping['groups'] = array( $map[ $subscription_id ] );
+        $interest_id = $map[ $subscription_id ];
+        $subscriber->interests[ $interest_id ] = true;
     }
 
-    // add to data array
-    $data['GROUPINGS'] = array( $grouping );
-    return $data;
+    // return modified subscriber object
+   return $subscriber;
 }
 
 
