6b75770a1dd5a6f5ec43936ee60dcd0ae81ec851
Danny van Kooten <dannyvankooten@gmail.com>
2017-02-06 14:55:07 +0100

    update snippets for latest ecommerce

--- add-ons/e-commerce/custom-order-status.php
+++ add-ons/e-commerce/custom-order-status.php
@@ -3,7 +3,7 @@
 /**
  * Add "wc-processing" to the order statuses which are sent to MailChimp
  */
-add_filter( 'mc4wp_ecommerce360_order_statuses', function( $statuses ) {
+add_filter( 'mc4wp_ecommerce_order_statuses', function( $statuses ) {
    $statuses[] = 'wc-processing';
    return $statuses;
 });
