02f2ec29c46ffdba7c6b4b4b565128de9f9e8de6
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-11-27 12:01:23 +0100

    delete snippet in incorrect place

--- add-ons/e-commerce/custom-order-status.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Add "wc-processing" to the order statuses which are sent to MailChimp
- */
-add_filter( 'mc4wp_ecommerce_order_statuses', function( $statuses ) {
-   $statuses[] = 'wc-processing';
-   return $statuses;
-});
