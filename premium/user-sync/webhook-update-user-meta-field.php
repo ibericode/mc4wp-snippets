40bb8a7efb72ee78b3b0f7f2e37104edee38c3fb
Arne Lap <arnelap@users.noreply.github.com>
2020-05-22 18:47:36 +0700

    Update webhook-update-user-meta-field.php

--- premium/user-sync/webhook-update-user-meta-field.php
+++ premium/user-sync/webhook-update-user-meta-field.php
@@ -1,5 +1,5 @@
 <?php
-
+//This will allow you to update a Wordpress user meta field with the Interest groups that are set in MailChimp  when syncing data from MailChimp to Wordpress via the 2 way sync webhook.
 add_action( 'mc4wp_user_sync_webhook', function($data, $user) {
     /* $data['merges'] contains an associative array with mailchimp field values */
     /* in this example we take the INTERESTS fields, which is a comma-separated string of interest groups */
