ea68ce8abe6b60d50cefe439845c91a0dfb72d5e
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-06-18 09:36:03 +0200

    turn snippet into plugin for easier usage

--- premium/ecommerce/only-if-email-already-on-list.php
+++ premium/ecommerce/only-if-email-already-on-list.php
@@ -1,17 +1,18 @@
 <?php
+/*
+Plugin Name: MailChimp for WordPress - Selectively send e-commerce data
+Plugin URI: https://mc4wp.com/
+Description: Only send e-commerce data to Mailchimp if email address is already on the selected Mailchimp list
+Author: ibericode
+Version: 1.0
+Author URI: https://ibericode.com/
+*/
 
 /**
- * Only send the customer to Mailchimp if they have status pending or subscribed (no transactional only emails)
- */
-
-/**
- * Check if a list member has status pending or subscribed based on their email.
- * It uses the list from the page MC4WP > E-commerce
+ * Check if a list member has status pending or subscribed, using the list from the MC4WP > E-commerce page
  *
  * @see https://mailchimp.com/developer/reference/lists/list-members/
- *
  * @param string $email
- *
  * @return bool `true` when the mailchimp list member has the status pending or subscribed
  */
 function mc4wp_mailchimp_list_member_has_status_pending_or_subscribed($email) {
@@ -77,7 +78,6 @@ function mc4wp_get_customer_email($customer) {
  *
  * @param bool $value the default value is `true`
  * @param WP_User|WC_Customer|object $customer
- *
  * @return bool whenever the customer is send to mailchimp on `true` it will be send
  */
 add_filter('mc4wp_ecommerce_send_cart_to_mailchimp', function ($value, $customer) {
@@ -91,7 +91,6 @@ add_filter('mc4wp_ecommerce_send_cart_to_mailchimp', function ($value, $customer
  *
  * @param bool $value the default value is `true`
  * @param WP_User|WC_Customer|object $customer
- *
  * @return bool whenever the customer is send to mailchimp on `true` it will be send
  */
 add_filter('mc4wp_ecommerce_send_customer_to_mailchimp', function ($value, $customer) {
@@ -105,7 +104,6 @@ add_filter('mc4wp_ecommerce_send_customer_to_mailchimp', function ($value, $cust
  *
  * @param bool $value the default value is `true`
  * @param WC_Order $order
- *
  * @return bool whenever the customer is send to mailchimp on `true` it will be send
  */
 add_filter('mc4wp_ecommerce_send_order_to_mailchimp', function ($value, WC_Order $order) {
