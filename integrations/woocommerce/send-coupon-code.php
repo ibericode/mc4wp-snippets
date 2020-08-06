f564a2a42256b76c48caa0e0f6aec7eb0e56f032
Harish Chouhan <me@harishchouhan.com>
2020-08-07 00:01:22 +0700

    Update send-coupon-code.php

--- integrations/woocommerce/send-coupon-code.php
+++ integrations/woocommerce/send-coupon-code.php
@@ -1,22 +1,17 @@
 <?php
 
 /**
- * This will send additional WooCommerce checkout fields to MailChimp.
+ * This will send the last coupon code used in the oder to MailChimp.
  *
  * @return array
  */
 add_filter( 'mc4wp_integration_woocommerce_data', function( $data, $order_id ) {
 	$order = wc_get_order( $order_id );
-	
     if( $order->get_used_coupons() ) {
-        
         foreach( $order->get_used_coupons() as $coupon) {
 	        $custom_coupon_code_field = $coupon;
         }
+        $data[ 'MMERGE5' ] = $custom_coupon_code_field;
     }
-
-	// if it's a custom checkout field, usually you can get its value like this:
-	$data[ 'NAME_OF_FIELD_IN_MAILCHIMP' ] = $custom_coupon_code_field;
-
 	return $data;
 }, 10, 2);
