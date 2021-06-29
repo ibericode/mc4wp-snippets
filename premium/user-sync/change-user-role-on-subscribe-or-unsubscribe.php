cd5664473db5cbfde7cc02c7e0cdbea1b0a55a67
Arne Lap <arnelap@users.noreply.github.com>
2021-06-29 17:42:43 +0700

    Update change-user-role-on-subscribe-or-unsubscribe.php

--- premium/user-sync/change-user-role-on-subscribe-or-unsubscribe.php
+++ premium/user-sync/change-user-role-on-subscribe-or-unsubscribe.php
@@ -1,7 +1,7 @@
 <?php
 // This snippet needs the  Webhook to be turned on under Mailchimp for WP > Usersync
 // It will change the user role when someone subscribes on unsubscribes from the Audience. 
-// Replace 'customer' and 'subscriber' with the slugs of the roles you are using for this. 
+// Replace 'unsubscribed' and 'subscriber' with the slugs of the roles you are using for this. 
 
 add_action( 'mc4wp_user_sync_webhook_unsubscribe', function($data, $user) {
 $user->remove_role( 'subscriber' );
