7770b2a4093f0a35c49c065d80743af4b5e88c0f
Arne Lap <arnelap@users.noreply.github.com>
2021-06-29 17:30:34 +0700

    Update change-user-role-on-subscribe-or-unsubscribe.php

--- premium/user-sync/change-user-role-on-subscribe-or-unsubscribe.php
+++ premium/user-sync/change-user-role-on-subscribe-or-unsubscribe.php
@@ -4,11 +4,11 @@
 // Replace 'customer' and 'subscriber' with the slugs of the roles you are using for this. 
 
 add_action( 'mc4wp_user_sync_webhook_unsubscribe', function($data, $user) {
-$user->remove_role( 'customer' );
-$user->add_role( 'subscriber' );
+$user->remove_role( 'subscriber' );
+$user->add_role( 'unsubscribed' );
 }, 10, 2);
 
 add_action( 'mc4wp_user_sync_webhook_subscribe', function($data, $user) {
-$user->remove_role( 'subscriber' );
-$user->add_role( 'customer' );
+$user->remove_role( 'unsubscribed' );
+$user->add_role( 'subscriber' );
 }, 10, 2);
