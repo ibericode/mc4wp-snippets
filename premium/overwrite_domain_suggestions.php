70b70e41378e8b84ee201339166d632204ad64cc
Arne Lap <arnelap@users.noreply.github.com>
2022-07-26 20:26:21 +0700

    Update overwrite_domain_suggestions.php

--- premium/overwrite_domain_suggestions.php
+++ premium/overwrite_domain_suggestions.php
@@ -1,10 +1,10 @@
-//*
+/*
 With the form setting "Enable autocomplete for email domains?" is set to Yes, domain name automcompletions are suggested when typing an email address into the email field of the form. 
 
 With the filter 'mc4wp_forms_email_domain_suggestions' you can overwrite what domains are suggested. The expected return for the filter is an array of domain names. 
 
 Note that the order of the domains in the array of domains is the order in which they are suggested. 
-*//
+*/
 
 
 //To add 1 domain to the suggestions without overwriting the others
